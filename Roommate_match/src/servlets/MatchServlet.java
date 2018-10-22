c46c4ee7ea493c9b4a6b3c2ec7cfe785cbfb909a
lazertazer <eisnerl@usc.edu>
2018-10-21 17:00:47 -0700

    match page template files

--- Roommate_match/src/servlets/MatchServlet.java
+++ Roommate_match/src/servlets/MatchServlet.java
@@ -7,15 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/MapServlet")
-public class MapServlet extends HttpServlet {
+@WebServlet("/MatchServlet")
+public class MatchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public MapServlet() {
+    public MatchServlet() {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 }
